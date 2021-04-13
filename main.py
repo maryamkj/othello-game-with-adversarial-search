481c88d8b0ff6ef617ed0baf94ed067152c36bf2
nika shahabi <nikashahabi@aut.ac.ir>
2021-04-13 05:04:50 +0430

    min max algorithm implemented

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import copy
+
 EMPTY, BLACK, WHITE = 0, 1, 2
 DIRECTIONS = ('up', 'down', 'right', 'left', 'upleft', 'upright', 'downleft', 'downright')
 
@@ -6,11 +8,11 @@ def initializeBoard(size):
     if size % 2 != 0:
         print("board can not be initialized. size is odd")
         return
-    half = int(size/2)
-    board[half][half] = WHITE
-    board[half][half+1] = BLACK
-    board[half+1][half] = BLACK
-    board[half+1][half+1] = WHITE
+    half = int(size/2) - 1
+    board[half][half] = BLACK
+    board[half][half+1] = WHITE
+    board[half+1][half] = WHITE
+    board[half+1][half+1] = BLACK
     return board
 
 
@@ -79,57 +81,133 @@ def getCell(current, direction):
 
 
 def findBracket(square, player, board, direction):
+    'finds a bracket for the player on the board on a given direction from a square(ke vasatesho por kone)'
     bracket = getCell(square, direction)
     (i, j) = bracket
-    if board[i][j] == player:
+    if isValid(board, bracket) is False or board[i][j] == player:
         return None
     opponent = getOpponent(player)
-    while board[i][j] == opponent:
+    while isValid(board, bracket) and board[i][j] == opponent:
         bracket = getCell(bracket, direction)
         (i, j) = bracket
     return None if isValid(board, bracket) is False or isEmpty(board, bracket) else bracket
 
 
+def nextPlayer(board, player):
+    opponent = getOpponent(player)
+    if hasLegalMoves(opponent, board):
+        return opponent
+    if hasLegalMoves(player, board):
+        return player
+    return None
 
 
-# def playOthello():
-#     board = initializeBoard(8)
-#     player = BLACK
-#     while player is not None:
-#         move = minmaxDecision(board, player)
-#         makeMove(move, board, player)
-#         player = nextPlayer(boad, player)
-#     return board, score(black, board)
-#
-#
-# def minmaxDecision(board, player):
-#     temp = -float("inf")
-#     for action in actions(board):
-#         if minValue(result(board, action), player) > temp:
-#             selectedAction = action
-#             temp = minValue(result(board, action), player)
-#     return selectedAction
-#
-#
-# def maxValue(board, player):
-#     if isTerminalState(board):
-#         return utility(board, player)
-#     temp = -float("inf")
-#     for action in actions(board):
-#         if minValue(result(board, action), player) > temp:
-#             temp = minValue(result(board, action), player)
-#     return temp
-#
-# def minValue(board, player):
-#     if isTerminalState(board):
-#         return utility(board, player)
-#     temp = float("inf")
-#     for action in actions(board):
-#         if maxValue((result(board, action), player) < temp:
-#             temp = maxValue(result(board, action), player)
-#     return temp
+def isLegal(board, player, move):
+    hasBracket = lambda direction: findBracket(move, player, board, direction)
+    (i, j) = move
+    return board[i][j] == EMPTY and any(map(hasBracket, DIRECTIONS))
 
-showBoard(initializeBoard(8))
-move = (0,1)
-board = initializeBoard(8)
+
+def hasLegalMoves(player, board):
+    size = len(board)
+    for i in range(size):
+        for j in range(size):
+            if isLegal(board, player, (i, j)):
+                return True
+    return False
+
+
+def legalMoves(board, player):
+    moves = []
+    size = len(board)
+    for i in range(size):
+        for j in range(size):
+            if isLegal(board, player, (i, j)):
+                moves.append((i, j))
+    'liste zoje morateb haaE ke player mitune unja bere'
+    return moves
+
+
+def showPlayer(player):
+    return "White" if player == WHITE else "Black"
+
+
+def score(board, player):
+    score, oppscore = 0, 0
+    opponent = getOpponent(player)
+    size = len(board)
+    for i in range(size):
+        for j in range(size):
+            if board[i][j] == player:
+                score += 1
+            if board[i][j] == opponent:
+                oppscore += 1
+    score = score - oppscore
+    return score
+
+
+def playOthello():
+    board = initializeBoard(8)
+    print("initial board:")
+    showBoard(board)
+    player = BLACK
+    while player is not None:
+        print("turn = ", showPlayer(player))
+        move = minmaxDecision(board, player)
+        print("the selected move is = ", move)
+        'pas minmax bayad khuneE ke mikhaym berim behesh ro bargardune'
+        makeMove(move, board, player)
+        print("board after the selected move")
+        showBoard(board)
+        player = nextPlayer(board, player)
+    print("no legal moves available.")
+    showBoard(board)
+    print("white's score =", score(board, WHITE))
+    print("black's score =", score(board, BLACK))
+    return board
+
+
+def result (board, action, player):
+    'action is a legal move. legal moves are zoje moratab haaE ke mishe be unja raft'
+    'mikhaym bebinim result esh chi mishe'
+    'board ro taghir nade'
+    newBoard = copy.deepcopy(board)
+    (i, j) = action
+    newBoard[i][j] = player
+    return newBoard
+
+
+def minmaxDecision(board, player):
+    temp = -float("inf")
+    for action in legalMoves(board, player):
+        if minValue(result(board, action, player), player) > temp:
+            selectedAction = action
+            temp = minValue(result(board, action, player), player)
+    return selectedAction
+
+
+def isTerminalState(board):
+    return True if hasLegalMoves(WHITE, board) is False and hasLegalMoves(BLACK, board) is False else True
+
+
+def maxValue(board, player):
+    if isTerminalState(board):
+        return score(board, player)
+    temp = -float("inf")
+    for action in legalMoves(board, player):
+        if minValue(result(board, action), player) > temp:
+            temp = minValue(result(board, action), player)
+    return temp
+
+
+def minValue(board, player):
+    if isTerminalState(board):
+        return score(board, player)
+    temp = float("inf")
+    for action in legalMoves(board, getOpponent(player)):
+        if maxValue((result(board, action), player)) < temp:
+            temp = maxValue(result(board, action), player)
+    return temp
+
+playOthello()
 
